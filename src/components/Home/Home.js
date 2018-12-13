2262f043b40835411c5d1ab1729e188a24d28e02
Andrii Didkivskyi <dorreharay@gmail.com>
2018-12-13 23:02:45 +0200

    zero webpack config with router

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className={styles.main}>
+
+      </div>
+    )
+  }
+}
+
+export default Home;

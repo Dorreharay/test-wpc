13d87138c369e14def6d62902ca82c6cce05f885
dorreharay <dorreharay@gmail.com>
2019-03-16 15:05:22 +0200

    added didmount

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,14 +8,16 @@ class Home extends Component {
   handleAsyncAction = () => {
     this.props.asyncAction()
   }
-
+  componentDidMount(){
+    this.props.asyncAction()
+  }
   
   render() {
     const { data } = this.props;
     return (
       <>
         <div onClick={this.handleAsyncAction} className="background">
-          {data && data[0].map((item) => <div>{item.title}</div>)}
+          {data && data[0].map((item) => <div key={item.id}>{item.title}</div>)}
         </div>
         <Loader loading={this.props.loading} />
       </>
